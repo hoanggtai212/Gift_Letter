94726bf772baf347358c12c77f046f2dde1c7b6f
HT <rongchube99@gmail.com>
2025-09-13 13:19:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   const audioEl = $("#sound")[0];
 
   let currentPage = 1;
-  const totalPages = 23;
+  const totalPages = 28;
   let isOpen = false;
   let hasPlayed = false;
   let isClosing = false; // guard để tránh đóng lặp
@@ -79,3 +79,4 @@ $(document).ready(function () {
       $("#page" + currentPage).addClass("active");
   }
 });
+
