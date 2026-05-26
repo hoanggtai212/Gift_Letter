25c824c1bb261ac610eb3317909fbe785f1adb06
HT <rongchube99@gmail.com>
2026-05-26 23:35:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   const audioEl = $("#sound")[0];
 
   let currentPage = 1;
-  const totalPages = 15;
+  const totalPages = 23;
   let isOpen = false;
   let hasPlayed = false;
   let isClosing = false; // guard để tránh đóng lặp
